22105faa03caffbbe0b3be210f2d732d92261015
Andrei Dodu <andrei.dodu@hotmail.it>
2022-08-04 09:19:38 +0200

    refactor: removed useless file

--- src/main/java/com/andreidodu/boilerplate/dao/DataBisDao.java
+++
@@ -1,9 +0,0 @@
-//package com.andreidodu.boilerplate.dao;
-//
-//import org.springframework.data.repository.CrudRepository;
-//
-//import com.andreidodu.boilerplate.db.DataBisDB;
-//
-//public interface DataBisDao extends CrudRepository<DataBisDB, Long> {
-//
-//}
