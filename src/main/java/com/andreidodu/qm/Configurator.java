991e8c7f8664ed354f80a964184ef2aa12479656
Andrei Dodu <andrei.dodu@hotmail.it>
2022-08-06 22:12:47 +0200

    refactor: for packaging

--- src/main/java/com/andreidodu/qm/Configurator.java
+++
@@ -1,8 +0,0 @@
-package com.andreidodu.qm;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class Configurator {
-
-}
--- src/main/java/com/andreidodu/qm/QuestionnaireManagerApplication.java
+++ src/main/java/com/andreidodu/qm/QuestionnaireManagerApplication.java
@@ -1,25 +1,13 @@
 package com.andreidodu.qm;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.andreidodu.qm.console.CommandProcessor;
-
 @SpringBootApplication
-public class QuestionnaireManagerApplication implements CommandLineRunner {
-
-	@Autowired
-	private CommandProcessor commandProcessor;
+public class QuestionnaireManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionnaireManagerApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-		this.commandProcessor.run();
 	}
 
 }
