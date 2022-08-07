55937674392e9d9b79a07d1ae72b95b86ccc64d2
Andrei Dodu <andrei.dodu@hotmail.it>
2022-08-07 12:50:20 +0200

    refactor: removed useless records

--- src/main/java/com/andreidodu/qm/dto/input/GroupInsert.java
+++
@@ -1,6 +0,0 @@
-package com.andreidodu.qm.dto.input;
-
-import com.andreidodu.qm.dto.input.common.CategoryInputCommon;
-
-public record GroupInsert(String code, String title, String help, String languageCode) implements CategoryInputCommon {
-}
--- src/main/java/com/andreidodu/qm/dto/input/SectionInsert.java
+++
@@ -1,6 +0,0 @@
-package com.andreidodu.qm.dto.input;
-
-import com.andreidodu.qm.dto.input.common.CategoryInputCommon;
-
-public record SectionInsert(String code, String title, String help, String languageCode) implements CategoryInputCommon {
-}
